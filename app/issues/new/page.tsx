4e7d7af093b0ce1737051823797ebf9b0ad71e63
michaell2182 <70760976+michaell2182@users.noreply.github.com>
2024-02-06 09:38:53 -0400

    new error message component

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,26 +1,44 @@
 "use client";
-import { Button, Callout, TextField, CalloutText } from "@radix-ui/themes";
+import {
+  Button,
+  Callout,
+  TextField,
+  CalloutText,
+  CalloutIcon,
+  Text,
+} from "@radix-ui/themes";
 import React, { useState } from "react";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/app/validationSchema";
+import { z } from "zod";
+import ErrorMessage from "@/app/components/ErrorMessage";
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>;
 const NewissuePage = () => {
   const router = useRouter();
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
   const [error, setError] = useState("");
 
   return (
     <div className="max-w-xl ">
-        {error && <Callout.Root color="red" className="mb-5">
-            <CalloutText>{error}</CalloutText>
-            </Callout.Root>}
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Icon />
+          <CalloutText>{error}</CalloutText>
+        </Callout.Root>
+      )}
       <form
         className=" space-y-3"
         onSubmit={handleSubmit(async (data) => {
@@ -35,6 +53,8 @@ const NewissuePage = () => {
         <TextField.Root>
           <TextField.Input placeholder="Title" {...register("title")} />
         </TextField.Root>
+      <ErrorMessage>{errors.title?.message}</ErrorMessage>
+
         <Controller
           name="description"
           control={control}
@@ -42,7 +62,9 @@ const NewissuePage = () => {
             <SimpleMDE placeholder="Description" {...field} />
           )}
         />
-
+       
+          <ErrorMessage>{errors.description?.message}</ErrorMessage>
+     
         <Button>Submit New Issue</Button>
       </form>
     </div>
