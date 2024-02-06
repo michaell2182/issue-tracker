f7b8909e1b371634bbcb1ab91be09fdb87b98517
michaell2182 <70760976+michaell2182@users.noreply.github.com>
2024-02-06 09:06:36 -0400

    error handling

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,24 +1,26 @@
 import { NextRequest, NextResponse } from "next/server";
-import { z } from 'zod';
+import { z } from "zod";
 import prisma from "@/prisma/client";
 import { title } from "process";
 import { describe } from "node:test";
+
+
 const createIssueSchema = z.object({
-   title: z.string().min(1).max(255),
-   description: z.string().min(1)
+  title: z.string().min(1, 'Title is required').max(255),
+  description: z.string().min(1, 'Description is required').max(255),
 });
 
-export async function POST(request:NextRequest) {
-    const body = await request.json();
-    const validation = createIssueSchema.safeParse(body);
-    if (!validation.success)
-    return NextResponse.json(validation.error.errors,{status:400})
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const validation = createIssueSchema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
 
-    const newIssue = await prisma.issue.create({
-        data:{
-            title:body.title, description:body.description
-        }
-    });
-    return NextResponse.json(newIssue,{status:201});
-
+  const newIssue = await prisma.issue.create({
+    data: {
+      title: body.title,
+      description: body.description,
+    },
+  });
+  return NextResponse.json(newIssue, { status: 201 });
 }
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,37 +1,52 @@
-'use client';
-import { Button,TextField } from '@radix-ui/themes'
-import React from 'react'
+"use client";
+import { Button, Callout, TextField, CalloutText } from "@radix-ui/themes";
+import React, { useState } from "react";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
-import {useForm, Controller} from 'react-hook-form';
-import axios from 'axios';
-import { useRouter } from 'next/navigation';
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
 interface IssueForm {
-    title: string;
-    description: string;
+  title: string;
+  description: string;
 }
 const NewissuePage = () => {
-   const router = useRouter();
-    const {register, control, handleSubmit} = useForm<IssueForm>();
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const [error, setError] = useState("");
 
   return (
-    <form className='max-w-xl space-y-3' onSubmit={handleSubmit(async (data)=>{
-        await axios.post('/api/issues', data);
-        router.push('/issues')
-    })}>
+    <div className="max-w-xl ">
+        {error && <Callout.Root color="red" className="mb-5">
+            <CalloutText>{error}</CalloutText>
+            </Callout.Root>}
+      <form
+        className=" space-y-3"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post("/api/issues", data);
+            router.push("/issues");
+          } catch (error) {
+            setError("An unexpected error occurred");
+          }
+        })}
+      >
         <TextField.Root>
-            <TextField.Input placeholder='Title'{...register('title')}/>
+          <TextField.Input placeholder="Title" {...register("title")} />
         </TextField.Root>
         <Controller
-        name="description"
-        control={control}
-        render={({field})=> <SimpleMDE placeholder='Description' {...field}/>}
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
         />
-        
-<Button>Submit New Issue</Button>
-    </form>
-  )
-}
 
-export default NewissuePage
+        <Button>Submit New Issue</Button>
+      </form>
+    </div>
+  );
+};
+
+export default NewissuePage;
