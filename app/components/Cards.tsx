c94b006e0bba724beafb5b5a068cf12c09f1565e
michaell2182 <70760976+michaell2182@users.noreply.github.com>
2024-02-13 20:30:37 -0400

    tweaks to the cards

--- app/components/Cards.tsx
+++ app/components/Cards.tsx
@@ -5,7 +5,7 @@ const Cards = () => {
     <main className="mt-6">
       <div className="grid grid-cols-3 gap-14 mt-6">
         {/* First Card */}
-        <div className="bg-white p-4 rounded-2xl h-48 w-72 shadow-md cursor-pointer  transition-colors  hover:bg-[#b4c2dc] hover:transform hover: ease-in-out duration-700">
+        <div className="bg-white p-4 rounded-2xl h-48 w-72 shadow-md cursor-pointer  transition ease-in-out delay-150 hover:bg-[#b4c2dc] hover:transform hover: hover:-translate-y-1 hover:scale-105 duration-300">
           <div className="flex justify-between items-center">
             <div>
               <h1 className="text-lg font-semibold center">Third Card</h1>
@@ -14,7 +14,7 @@ const Cards = () => {
         </div>
 
         {/* Second Card */}
-        <div className="bg-white p-4 rounded-2xl h-48 w-72 shadow-md cursor-pointer  transition-colors  hover:bg-[#b4c2dc] hover:transform hover: ease-in-out duration-700">
+        <div className="bg-white p-4 rounded-2xl h-48 w-72 shadow-md cursor-pointer  transition ease-in-out delay-150 hover:bg-[#b4c2dc] hover:transform hover: hover:-translate-y-1 hover:scale-105 duration-300">
           <div className="flex justify-between items-center">
             <div>
               <h1 className="text-lg font-semibold center">Third Card</h1>
@@ -23,7 +23,7 @@ const Cards = () => {
         </div>
 
         {/* Third Card */}
-        <div className="bg-white p-4 rounded-2xl h-48 w-72 shadow-md cursor-pointer  transition-colors  hover:bg-[#b4c2dc] hover:transform hover: ease-in-out duration-700">
+        <div className="bg-white p-4 rounded-2xl h-48 w-72 shadow-md cursor-pointer  transition ease-in-out delay-150 hover:bg-[#b4c2dc] hover:transform hover: hover:-translate-y-1 hover:scale-105 duration-300">
           <div className="flex justify-between items-center">
             <div>
               <h1 className="text-lg font-semibold center">Third Card</h1>
